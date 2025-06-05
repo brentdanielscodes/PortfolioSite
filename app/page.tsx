dfaa00441aaef54ac876a2b6e81dfb8f166f3c69
brentdanielscodes <127131669+brentdanielscodes@users.noreply.github.com>
2025-06-04 22:07:57 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,21 @@ import Sidebar from "./components/Sidebar";
 import AboutCard from "./components/AboutCard";
 import Services from "./components/Services";
 
+export const metadata = {
+  title: "BD Portfolio",
+  description: "Portfolio of Brent Daniels, Full Stack Engineer",
+};
+
 export default function Home() {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
       <main
         className="flex-1 p-6 bg-cover bg-center"
-        style={{ backgroundImage: 'url(https://www.charleston-sc.gov/ImageRepository/Document?documentID=23032)' }}
+        style={{
+          backgroundImage:
+            "url(https://www.charleston-sc.gov/ImageRepository/Document?documentID=23032)",
+        }}
       >
         <AboutCard />
         <Services />
