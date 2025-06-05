ebc54f550c6bfec3f1c09e4dd367b30cf7362bd3
brentdanielscodes <127131669+brentdanielscodes@users.noreply.github.com>
2025-06-04 21:43:13 -0400

    Update AboutCard.tsx

--- app/components/AboutCard.tsx
+++ app/components/AboutCard.tsx
@@ -4,9 +4,9 @@ export default function AboutCard() {
     <section id="about" className="bg-white p-6 rounded shadow mb-6">
       <h2 className="text-xl font-bold text-blue-600 mb-2">About Me</h2>
       <p className="text-gray-700 mb-4">
-  <strong>Hello! I'm Brent Daniels.</strong> Full stack engineer from Lexington, South Carolina. I have rich
-  experience in both frontend and backend development, and I’m also skilled in WordPress. I am open to discussing employment opportunities and exciting projects.
-</p>
+        <strong>Hello! I&apos;m Brent Daniels.</strong> Full stack engineer from Lexington, South Carolina. I have rich
+        experience in both frontend and backend development, and I&apos;m also skilled in WordPress. I am open to discussing employment opportunities and exciting projects.
+      </p>
       <ul className="text-sm text-gray-600">
         <li><strong>Age:</strong> 21</li>
         <li><strong>Residence:</strong> Charleston, SC, USA</li>
